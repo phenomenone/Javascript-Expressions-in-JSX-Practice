5d9c1bfd3558e10335112065f5f811dd0a7a8b7b
Eden Castillo Asport <35803612+phenomenone@users.noreply.github.com>
2022-04-28 17:58:47 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div></div>, document.getElementById("root"));
